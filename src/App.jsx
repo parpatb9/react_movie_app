054e248321623b1a4ecdaa3fb189aed6e90b1720
parpatb9 <patelparth1960@gmail.com>
2025-04-22 12:50:17 +1200

    setup Appwrite databse

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce} from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY= import.meta.env.VITE_TMBD_API_KEY;
@@ -43,6 +44,11 @@ const App = () => {
       }
       const data = await response.json();
       setMovieList(data.results || []);
+
+    if(query && data.results.length > 0){
+     
+      await updateSearchCount(query, data.results[0]);
+    }
    
     } catch (error) {
       console.error('Error fetching movies:', error);
---
+++ src/appwrite.js
@@ -0,0 +1,42 @@
+import { Client, Databases, ID, Query } from 'appwrite';
+
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABSE_ID;
+
+const client = new Client()
+  .setEndpoint('https://fra.cloud.appwrite.io/v1')
+  .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+export const updateSearchCount = async(searchTerm, movie)=>{
+   // 1. use Appwrite SDK to check if the search term exists in the database
+   try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', searchTerm),
+        ])
+
+        if(result.documents.length > 0){
+            // If it exists, update the count
+            const doc = result.documents[0];
+            // Update the count by incrementing it by 1
+            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+                count: doc.count + 1,
+            });
+        }
+        else{
+            // If it doesn't exist, create a new document with the search term and count 1
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                searchTerm,
+                count: 1,
+                movie_id: movie.id,
+                poster_url : `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+            });
+        }
+
+   } catch (error) {
+        console.error('Error updating search count:', error);
+   }
+   // 2. If it exists, update the count
+   // 3. If it doesn't exist, create a new document with the search term and count 1
+}
